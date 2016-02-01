a25ed13f2af49eb3ec0008882a92d0ab9b316c5e
Lindsay Ward <lindsay.ward@jcu.edu.au>
2016-02-01 15:01:55 +1000

    Added basic functions demo as starter point

---
+++ basic_functions.py
@@ -0,0 +1,25 @@
+"""
+CP1404/CP5632 Workshop 04
+Basic functions
+demonstrating various parameters, returns and the use of a main function
+"""
+__author__ = 'Lindsay Ward'
+
+
+def main():
+    lowest, highest = get_limits()
+    print("lowest =", lowest, "highest =", highest)
+    print_between(lowest, highest)
+
+
+def get_limits():
+    minimum = int(input("Enter the minimum: "))
+    maximum = int(input("Enter the maximum: "))
+    return minimum, maximum
+
+
+def print_between(start, end):
+    for i in range(start, end+1):
+        print(i, end=' ')
+
+main()
