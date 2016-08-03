a288b6345123cd371cc98389c0d2f5a6f238457f
Lindsay Ward <lindsay.ward@jcu.edu.au>
2016-08-03 17:24:00 +1000

    Add .gitignore, __main__ check, Python version and Wiki link

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Starter
-Code repository with basic starter Python code for CP1404/CP5632 @ JCU
+Code repository with basic starter Python code for CP1404/CP1804/CP5632 @ JCU
 
-See the Wiki for instructions to setup Python, Git, Kivy on your Windows or Mac PC.
+See the Wiki for instructions to setup Python, Git, GitHub and Kivy on your Windows or Mac PC.  
+https://github.com/CP1404/Starter/wiki/Software-Setup
--- check_setup.py
+++ check_setup.py
@@ -1,11 +1,14 @@
 """
 Basic Kivy test code
 If this works, it will produce a GUI window with "hello world" in the middle
-and you know you have set everything up correctly
+and you know you have set everything up correctly.
+It also prints the Python version in the run output (may be mixed in with Kivy output)
+The Python version should be 3.x (not 2.x).
 """
 
 from kivy.app import App
 from kivy.uix.button import Button
+import sys
 
 
 class TestApp(App):
@@ -13,5 +16,8 @@ class TestApp(App):
     def build(self):
         return Button(text='hello world')
 
-TestApp().run()
 
+if __name__ == '__main__':
+    # In PyCharm, right-click here and choose 'Run check_setup'
+    print("Python version information:", sys.version)
+    TestApp().run()
