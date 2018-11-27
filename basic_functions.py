9450d971c8a62c5a6ad790d9d5cd419b976a964b
Lindsay Ward <lindsay.ward@jcu.edu.au>
2018-11-27 15:19:51 +1000

    Update basic functions demo with docstrings

--- basic_functions.py
+++ basic_functions.py
@@ -1,29 +1,27 @@
-
-
-
-
 """
-CP1404/CP5632 Workshop 04
-Basic functions
-demonstrating various parameters, returns and the use of a main function
+CP1404/CP5632 - Basic functions
+Demonstrating various parameters, returns and the use of a main function
 """
-__author__ = 'Lindsay Ward'
 
 
 def main():
+    """Demo of simple program with functions."""
     lowest, highest = get_limits()
     print("lowest =", lowest, "highest =", highest)
     print_between(lowest, highest)
 
 
 def get_limits():
+    """Get minimum and maximum numbers (integer limits)."""
     minimum = int(input("Enter the minimum: "))
     maximum = int(input("Enter the maximum: "))
     return minimum, maximum
 
 
 def print_between(start, end):
-    for i in range(start, end+1):
+    """Print integers between start and end."""
+    for i in range(start, end + 1):
         print(i, end=' ')
+
 
 main()
